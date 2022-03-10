03f6fcfb267e7808af5a606ed10036713dc078d7
Brian Wandell <wandell@stanford.edu>
2022-03-10 09:21:25 -0800

    Update s_copyProject.m

--- examples/s_copyProject.m
+++ examples/s_copyProject.m
@@ -3,15 +3,36 @@
 
 %% API Keys
 source      = 'cni.stanford.edu';
-destination = 'stanfordlabs.flywheel.io';
-sourcekey        = 'cni.flywheel.io:HYsHA7Gs8K9ySHRvsz';
-destinationkey   = 'stanfordlabs.flywheel.io:OQlKDTTTlkZsmDDQi_Pz3r1-_2E1m5D24REuP_3LNggUrwEkMVI1Up3i';
+sourcekey   = 'cni.flywheel.io:HYsHA7Gs8K9ySHRvsz';
+sourceroute = 'fw://cni/Psych204a';
 
-% Sign in to the destination
+destination        = 'stanfordlabs.flywheel.io';
+destinationkey     = 'stanfordlabs.flywheel.io:OQlKDTTTlkZsmDDQi_Pz3r1-_2E1m5D24REuP_3LNggUrwEkMVI1Up3i';
+destinationgroup   = 'wandell';
+destinationproject = 'Psych204a';
+
+%% Show the fw version
+[s,r] = system('fw version');
+disp(r(1:(end-5)))
+
+%% Sign in to the destination
 cmd = sprintf('fw login %s:%s',destination,destinationkey);
-[status, result] = system(cmd);
+[s, r] = system(cmd);
 
-system('fw status');
+% Verify login status
+[s,r] = system('fw status');
+disp(r(1:(end-5)))
+
+%% Build the command
+cmd = sprintf('fw ingest project --group %s --project %s %s %s',destinationgroup,destinationproject,sourceroute,sourcekey);
+
+fprintf('Copy and paste this command into a terminal window\n');
+disp(cmd)
+
+% We need to get the question returned and answer yes.  I am not sure how.
+%
+%  [s,r] = system(cmd);
 
 % Project on CNI (source):   fw://cni/Psych204a
 % Project on SL  (destination):  
+
