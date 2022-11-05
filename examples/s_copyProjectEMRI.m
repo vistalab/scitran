97514663f83d39813d24002c09ac4613aec6160d
Brian Wandell <wandell@stanford.edu>
2022-11-05 10:23:03 -0700

    Another copy example

--- examples/s_copyProjectEMRI.m
+++ examples/s_copyProjectEMRI.m
@@ -1,21 +1,36 @@
 %% Example of copying a project from FW-A to FW-B
 %
+% Copy of the eMRI data on CNI to Stanfordlabs.  Maybe.
+%
 % The instructions for how to do this are on the FLywheel manual page at
 %
 %    https://docs.flywheel.io/hc/en-us/articles/360059289793
 %
+%
+% Make sure fw is on your path
+%
+%   setenv('PATH', [getenv('PATH') ':/usr/local/bin']);
 
 %% API Keys
+%
+% Odd that the sourcekey has cni.flywheel.io but the destinationkey does
+% not.
 source      = 'cni.stanford.edu';
 sourcekey   = 'cni.flywheel.io:HYsHA7Gs8K9ySHRvsz';
-sourceroute = 'fw://cni/Psych204a';
+sourceroute = 'fw://jlg/DIANA';
 
 destination        = 'stanfordlabs.flywheel.io';
-destinationkey     = 'stanfordlabs.flywheel.io:OQlKDTTTlkZsmDDQi_Pz3r1-_2E1m5D24REuP_3LNggUrwEkMVI1Up3i';
+destinationkey     = 'OQlKDTTTlkZsmDDQi_Pz3r1-_2E1m5D24REuP_3LNggUrwEkMVI1Up3i';
 destinationgroup   = 'wandell';
-destinationproject = 'Psych204a';
+destinationproject = 'eMRI';
 
 %% Show the fw version
+% The system command is returning an error on my home computer, 
+%
+%    zsh:1: command not found
+%  
+% Why?
+
 [s,r] = system('fw version');
 disp(r(1:(end-5)))
 
---
+++ examples/s_fwInstall.m
@@ -0,0 +1,24 @@
+%% s_fwInstall
+%
+% To download the current CLI, go to the Flywheel site and look under your
+% Profile.  Choose the download for your system type.
+%
+%% If you are on a Mac
+% You can download and install it.  However, it will
+% not run immediately because it is not trusted by the MacOS.
+%
+% To make it trusted you can
+%
+%  https://news.trendmicro.com/2021/12/16/how-to-fix-macos-cannot-verify-that-this-app-is-free-from-malware-error/
+%
+% Option 1: Using Control Click
+%   1. Open Finder on your Mac.
+%   2. Select the app with "macOS cannot verify that this app is free from malware" error.
+%   3. Control-Click on the application to open the shortcut menu.
+%
+% Or
+% Option 2: Open Anyway
+%   1. On the Apple menu, select System Preferences.
+%   2. Click Security & Privacy and tap the General tab.
+%   3. You should see an "Open Anyway" button for the unverified app. This option only stays for about an hour after you try to open the blocked app.
+%
